6d4d901e3aa339ade676aae7ac4bc1d00495b99e
Jamie Benjamin <bennythedev@gmail.com>
2022-08-31 17:16:01 -0500

    admin product table

--- client/src/AdminProductView.js
+++ client/src/AdminProductView.js
@@ -1,34 +1,26 @@
-import { Table } from 'semantic-ui-react'
+import { Table, Container } from 'semantic-ui-react'
 
 function AdminProductView({product}){
     return(
-    // <Table fixed>
-    //     <Table.Header>
-    //         <Table.Row>
-    //             <Table.HeaderCell>Product Title</Table.HeaderCell>
-    //             <Table.HeaderCell>Description</Table.HeaderCell>
-    //             <Table.HeaderCell>Image</Table.HeaderCell>
-    //         </Table.Row>
-    //     </Table.Header>
+        <Container>
+            <Table fixed>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Product Title</Table.HeaderCell>
+                        <Table.HeaderCell>Description</Table.HeaderCell>
+                        <Table.HeaderCell>Image</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
 
-    //     <Table.Body>
-    //         <Table.Row>
-    //             <Table.Cell>{product.title}</Table.Cell>
-    //             <Table.Cell>{product.image}</Table.Cell>
-    //             <Table.Cell>{product.description}</Table.Cell>
-    //         </Table.Row>
-    //         <Table.Row>
-    //         <Table.Cell>{product.title}</Table.Cell>
-    //             <Table.Cell>{product.image}</Table.Cell>
-    //             <Table.Cell>{product.description}</Table.Cell>
-    //         </Table.Row>
-    //         <Table.Row>
-    //         <Table.Cell>{product.title}</Table.Cell>
-    //             <Table.Cell>{product.image}</Table.Cell>
-    //             <Table.Cell>{product.description}</Table.Cell>
-    //         </Table.Row>
-    //     </Table.Body>
-    // </Table>
+                <Table.Body>
+                    <Table.Row>
+                        <Table.Cell>{product.title}</Table.Cell>
+                        <Table.Cell>{product.description}</Table.Cell>
+                        <Table.Cell>{product.image}</Table.Cell>
+                    </Table.Row>
+                </Table.Body>
+            </Table>
+        </Container>
 
         // <div>
         //     <p>{product.title}</p>
