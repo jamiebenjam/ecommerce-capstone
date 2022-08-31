1328d2ec6f07f9edf8e6f5e6fe17410f099a6d38
Jamie Benjamin <bennythedev@gmail.com>
2022-08-31 12:46:24 -0500

    remove layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,8 @@
 import './App.css';
-import HomePageFooter from './HomePageFooter';
-import HomePageArticles from './HomePageArticles';
-import HomePageReviews from './HomePageReviews';
-import HomePageDescription from './HomePageDescription';
-import HomePageHeadingMenu from './HomePageHeadingMenu';
 
 function App() {
   return (
     <div className="App">
-      <HomePageHeadingMenu />
-      <HomePageDescription />
-      <HomePageReviews />
-      <HomePageArticles />
-      <HomePageFooter />
     </div>
   );
 }
